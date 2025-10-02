d82601a93e2d759362990e6527677f4f466d6908
Max BENICHOU <mbenicho@f0r12s16.clusters.42paris.fr>
2025-10-02 13:09:20 +0200

    sa race

--- bonus/ft_list_sort.s
+++ bonus/ft_list_sort.s
@@ -3,6 +3,34 @@ section	.text
 global	ft_list_sort
 
 ft_insert:
+	mov		rdx,rcx
+	jmp		insert_end
+
+	insert_loop:
+	mov		rdi,r9
+	mov		rsi,rcx
+	push	rdx
+	push	rcx
+	push	r9
+	call	r8
+	pop		r9
+	pop		rcx
+	pop		rdx
+
+	cmp		rax,0
+	jle		insert_end
+
+	mov		rdx,rcx
+	mov		rcx,[rcx+8]
+
+	cmp		rcx,0
+	je		insert_end
+
+	jmp		insert_loop
+
+	insert_end:
+	mov		[r9+8],rcx
+	mov		rdx,r9
 	ret
 
 
@@ -42,7 +70,11 @@ ft_list_sort:
 	mov		r9,rdx
 	mov		rdx,[rdx+8]
 	mov		qword[r9+8],0
+	push	rdx
+	push	rcx
 	call	ft_insert
+	pop		rcx
+	pop		rdx
 	;jmp		main_loop
 
 	end:
