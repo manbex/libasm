957923574d993a57ccadb5617498e30940f90eb8
Max <max@manbex.fr>
2025-11-27 17:58:36 +0100

    function prototype changed from int to void

--- main_bonus.c
+++ main_bonus.c
@@ -16,7 +16,7 @@ typedef struct s_list
 void	ft_list_push_front(t_list **begin_list, void *data);
 int		ft_list_size(t_list *begin_list);
 void	ft_list_sort(t_list **begin_list, int (*cmp)());
-int		ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)(), void (*free_fct)(void *));
+void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)(), void (*free_fct)(void *));
 
 
 void	ft_print_list(t_list *lst)
